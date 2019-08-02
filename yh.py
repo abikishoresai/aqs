ce8447dbed9a2f7b171ae0b8ed610f5282ecd3b9
abikishoresai <52270874+abikishoresai@users.noreply.github.com>
2019-08-02 16:43:41 +0530

    Create yh.py

---
+++ yh.py
@@ -0,0 +1,15 @@
+    
+t=int(input())
+s=""
+for i in range(2,n):
+    if t%i==0:
+        flag=0
+        break
+    else:
+        flag=1
+if flag==0:
+    print("yes")
+else:
+    print("no")
+© 2019 GitHub, Inc.
+}
